0c3e26662a2cca6185d66a2c8c61f4582e7f77a8
Jannik Buschke <jannik.buschke@gmail.com>
2019-08-08 12:05:21 +0200

    expose config ad a isLoggedIn function

--- src/MsalContext.tsx
+++ src/MsalContext.tsx
@@ -5,15 +5,34 @@ interface MsalContext {
     login: (request?: MSAL.AuthenticationParameters) => void | Promise<any>;
     logout: () => void;
     acquireToken: (request: MSAL.AuthenticationParameters) => Promise<MSAL.AuthResponse>;
+    config: MSAL.Configuration;
+    isLoggedIn: () => Promise<boolean>;
 }
 
 type LoginMethod = "redirect" | "popup"
 
-
 export const MsalContext = React.createContext<MsalContext>(undefined as any);
 
-export function MsalProvider({ config, children, loginMethod, defaultLoginParameters }: { config: MSAL.Configuration, children: React.ReactNode, loginMethod: LoginMethod, defaultLoginParameters: MSAL.AuthenticationParameters }) {
-    const app = React.useMemo(() => new MSAL.UserAgentApplication(config), [])
+interface Props
+{
+  config: MSAL.Configuration
+  children: React.ReactNode
+  loginMethod: LoginMethod
+  defaultLoginParameters: MSAL.AuthenticationParameters
+}
+
+export function MsalProvider({ config, children, loginMethod, defaultLoginParameters }: Props) {
+    const app = React.useMemo(() => {
+        const agent = new MSAL.UserAgentApplication(config)
+        function authCallback(error:any, response:any) {
+            console.error("msal redirect error")
+            console.error(error)
+            console.error(response)
+        }
+        // (optional when using redirect methods) register redirect call back for Success or Error
+        agent.handleRedirectCallback(authCallback);
+        return agent
+    }, [])
 
     async function login(request: MSAL.AuthenticationParameters) {
         if (loginMethod === "popup") {
@@ -26,30 +45,44 @@ export function MsalProvider({ config, children, loginMethod, defaultLoginParame
     }
 
     return <MsalContext.Provider value={{
+        config,
         login: login,
         logout: () => {
             app.logout()
         },
         acquireToken: async (request) => {
             try {
-                const token = await app.acquireTokenSilent({
-                    authority: app.authority,
+                const $request = {
+                    // authority: app.authority,
                     ...request,
-                });
-                console.log("got token", token)
+                }
+                const token = await app.acquireTokenSilent($request);
                 return token;
             } catch (E) {
                 if (E.errorCode === "user_login_error") {
                     await login(request);
-                } else {
-                    console.error("errorcode:",E.errorCode)
-                    throw E
+                }
+                else if(E.errorCode ==="token_renewal_error") {
+                    await login(request);
+                }
+                else {
+                    console.error("errorcode:", E.errorCode)
+                    throw new Error("Could not acquire token silent " + E.toString())
                 }
                 const token = await app.acquireTokenSilent({
                     authority: app.authority,
                     ...request,
                 });
                 return token;
+            }
+        },
+        isLoggedIn: async (scopes?: string[]) => {
+            try {
+                await app.acquireTokenSilent({ scopes: scopes? scopes: ["User.Read"] });
+                return true
+            } catch (E) {
+                console.log("checking for login error",E)
+                return false
             }
         }
     }}> {children}</MsalContext.Provider >
